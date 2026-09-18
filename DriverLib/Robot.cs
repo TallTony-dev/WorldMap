ea5a81ff2ef833749046e1e6b9876d6d52329603
Talltony-dev <anthonyabbass@icloud.com>
2026-09-17 20:34:36 -0400

    m

--- DriverLib/Robot.cs
+++ DriverLib/Robot.cs
@@ -34,7 +34,7 @@ namespace DriverLib
         }
 
 
-        public async Task MoveInDirectionUntilSense(Direction movementDir, Direction objectDirection, int distanceFromObject)
+        public async Task MoveInDirectionUntilSenseForwards(Direction movementDir, int distanceFromObject)
         {
             
         }
